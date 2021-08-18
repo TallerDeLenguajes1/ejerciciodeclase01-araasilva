080678fdee5b598176e82c9bc332a04dea3eed9a
Araceli <araasilva96@gmail.com>
2021-08-18 18:10:13 -0300

    ejercicio1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class adivinarNum
+    {
+       
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int numeroRandom = rand.Next(0, 100);
+            Console.WriteLine("Ingrese el numero: ");
+            int numeroUsuario = Convert.ToInt32(Console.ReadLine());
+
+            while (EvaluarPosicion(numeroUsuario, numeroRandom) == false)
+            {
+                Console.WriteLine("Ingrese el numero: ");
+                numeroUsuario = Convert.ToInt32(Console.ReadLine());
+
+            }
+        }
+
+        public static bool EvaluarPosicion(int n, int m)
+        {
+            if (n > m)
+            {
+                Console.WriteLine("el numero es mas grande");
+            }
+            if (n < m)
+            {
+                Console.WriteLine("el numero es mas chico");
+            }
+            if (n == m)
+            {
+                Console.WriteLine("encontraste el numero");
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
+
